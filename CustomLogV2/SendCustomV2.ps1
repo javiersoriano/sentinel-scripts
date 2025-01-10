abdb993a839cad07a3d17a8781f318c024ce16fc
Javier Soriano <javiersoriano@users.noreply.github.com>
2025-01-10 12:51:23 +0100

    Update SendCustomV2.ps1

--- CustomLogV2/SendCustomV2.ps1
+++ CustomLogV2/SendCustomV2.ps1
@@ -2,13 +2,13 @@
 ### Step 0: set parameters required for the rest of the script
 ##################
 #information needed to authenticate to AAD and obtain a bearer token
-$tenantId = "5f1060f2-d9a4-4f59-bf9c-1dd8f3604a4b"; #the tenant ID in which the Data Collection Endpoint resides
-$appId = "6a7b9c6d-c02c-458a-af79-c44caee2f698"; #the app ID created and granted permissions
-$appSecret = "8n~7Q~LHDu-CiwxBWwFoZaXKW1LUDlVjv8b5a"; #the secret created for the above app
+$tenantId = "*************************"; #the tenant ID in which the Data Collection Endpoint resides
+$appId = "***********************"; #the app ID created and granted permissions
+$appSecret = ******************"; #the secret created for the above app
 
 #information needed to send data to the DCR endpoint
-$dcrImmutableId = "dcr-7f059bd250854f189c791182685b3f21"; #the immutableId property of the DCR object
-$dceEndpoint = "https://dce-test-6u1t.westus2-1.ingest.monitor.azure.com"; #the endpoint property of the Data Collection Endpoint object
+$dcrImmutableId = "dcr-****************"; #the immutableId property of the DCR object
+$dceEndpoint = "https://************.ingest.monitor.azure.com"; #the endpoint property of the Data Collection Endpoint object
 
 ##################
 ### Step 1: obtain a bearer token that we'll later use to authenticate against the DCR endpoint
